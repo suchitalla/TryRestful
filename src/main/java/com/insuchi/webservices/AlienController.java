1d2538cbedefb7b4035eaddc37f59c4f59f15106
suchitalla <suchitalla@yahoo.com>
2019-05-15 15:29:19 -0400

    working trial 3

--- src/main/java/com/insuchi/webservices/AlienController.java
+++ src/main/java/com/insuchi/webservices/AlienController.java
@@ -1,5 +1,7 @@
 package com.insuchi.webservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +11,20 @@ public class AlienController {
 	@Autowired
 	AlienRepository aRep;
 	
+	//public static final Logger LOGGER = LoggerFactory.getLogger(AlienController.class);
 
 
 	@RequestMapping("/")
 	public String home()
 	{
+		//LOGGER.debug("In home");
 		return "homes";
 	}
 	@RequestMapping("/addAlien")
 	public String addAlien(Alien alien)
 	{ 
-		alien.setAid(1009);
+		//LOGGER.debug("In addAlien Suchi *************************************");
+		//alien.setAid(1009);
 		aRep.save(alien);
 		return "homes";
 	}
